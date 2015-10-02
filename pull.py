f07c2ccbcb9a25dc32748cd0b87f64fdbcecfe7e
Lucas Åström <lucasas@kth.se>
2015-10-02 16:23:02 +0200

    restructure pull script to take better care of existing and non-existing folders

--- pull.py
+++ pull.py
@@ -9,33 +9,36 @@ tag = ""
 if len(sys.argv) > 1:
     tag = "--branch %s " % sys.argv[1]
 
-dir = os.getcwd() + "/"
+dir = os.getcwd()
 
 with open("deps.txt") as f:
-    for line in f:
-        folder = line.split("/")[-1:][0][:-5]
-        call(("mkdir -p %s" % (folder)).split(" "))
-        os.chdir(folder)
-        unpushed = check_output("git log origin/master..master".split(" "))
-        uncommitted = check_output("git status --porcelain".split(" "))
+    for line_untrimmed in f:
+        line = line_untrimmed.strip()
+        folder = line.split("/")[-1:][0][:-4]
         problem = ""
         problem_text = ""
+        if os.path.isdir(folder):
+            call(("mkdir -p %s" % (folder)).split(" "))
 
-        if unpushed:
-            problem = "unpushed"
-            problem_text = problem_text + "Unpushed:\n" + unpushed + "\n"
-        if uncommitted:
-            problem = "uncommitted"
-            problem_text = problem_text + "Uncommitted:\n" + uncommitted + "\n"
+            os.chdir(folder)
+            unpushed = check_output("git log origin/master..master".split(" "))
+            uncommitted = check_output("git status --porcelain".split(" "))
 
-        if problem:
-            print problem_text
-            choice = raw_input("%s has %s changes! Do you wish to overwrite changes? (y/n) " % (folder, problem))
-            while choice != "y" and choice != "n":
-                choice = raw_input("Make a choice, nerd! (y/n) ")
-            if choice == "n":
-                print "Aborting..."
-                sys.exit(0)
+            if unpushed:
+                problem = "unpushed"
+                problem_text = problem_text + "Unpushed:\n" + unpushed + "\n"
+            if uncommitted:
+                problem = "uncommitted"
+                problem_text = problem_text + "Uncommitted:\n" + uncommitted + "\n"
+
+            if problem:
+                print problem_text
+                choice = raw_input("%s has %s changes! Do you wish to overwrite changes? (y/n) " % (folder, problem))
+                while choice != "y" and choice != "n":
+                    choice = raw_input("Make a choice, nerd! (y/n) ")
+                if choice == "n":
+                    print "Aborting..."
+                    sys.exit(0)
 
         os.chdir(dir)
         call(("rm -R -f %s" % (folder)).split(" "))
